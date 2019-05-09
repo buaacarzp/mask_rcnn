da5940e6916c5e603abdd9e2beb14c2ca8167e02
buaacarzp <43240561+buaacarzp@users.noreply.github.com>
2019-05-09 23:28:25 +0800

    fomat_data_v3
    this file can copy the pic  and shufut the file, fix the v2

--- format_data.py
+++ format_data.py
@@ -7,12 +7,14 @@ Created on 2019.4.27
 
 import os,shutil
 
-file_path="./train_datas/labelme_json/"
-new_traindata="./new_traindata/"
+file_path="./train_datasets/labelme_json/"
+new_traindata="./new_train_datasets/"
 trash="./trash"
-cv2_mask="./cv2_mask"
+cv2_mask="./new_train_datasets/cv2_mask"
+new_file_path_pic=new_traindata+"pic/"
 list_list=os.listdir(file_path)
 file_counts=len(list_list)
+trash_cnt=0
 print("在"+str(file_path)+"下的文件个数为：" +str(file_counts))
 
 
@@ -25,6 +27,7 @@ for i in range (file_counts):
     if os.path.exists(file_path+str(i+1)+"_json/"): 
         count = len(open(file_path_label, 'r').readlines())    
         print(str(i+1)+"_json's "+"line:",count)
+    
     if count!=2:
         print("TODO:移动"+str(i+1)+"_json文件夹")
         '''
@@ -34,7 +37,10 @@ for i in range (file_counts):
         '''
         if not os.path.exists(trash):
             os.mkdir(trash)
+            print("create a new floder: trash")
         shutil.move(file_path+str(i+1)+"_json",trash)
+        trash_cnt+=1
+print("error file's counts are :",trash_cnt)
 print("删除后的文件总数是：",len(os.listdir(file_path)))
 
 
@@ -58,17 +64,29 @@ for i in range(file_counts):
     file_count=list_list[0].split('_')[0]
     file_path_label=new_traindata+str(i+1)+"_json/"+"label.png"#此处可以优化为寻找以.png格式的文件路径
 #    if os.path.isfile(file_path_label):
+    file_path_pic=new_traindata+str(i+1)+"_json/"+"img.png"
     if 1:
         new_file_path_label=new_traindata+str(i+1)+"_json/"+str(i+1)+".png"
         os.rename(file_path_label,new_file_path_label)
         '''
         将new_file_path_label复制到cv2_mask目录下
         '''
+        '''
+        for convinent, I let the new_traindata /_json / img.png copy to the pic file and rename it as 1.2.3.4.5..bmp
+        '''
         if not os.path.exists(cv2_mask):
             os.mkdir(cv2_mask)
+            print("create the cv2_mask folder")
+        if not os.path.exists(new_file_path_pic):
+            os.mkdir(new_file_path_pic)
+            print("create the new pic folder")
         #shutil.move(new_file_path_label,cv2_mask)
         shutil.copy(new_file_path_label,cv2_mask)
+        shutil.copy(file_path_pic,new_file_path_pic)
+        #os.rename(new_file_path_pic+"img.png",new_file_path_pic+str(i+1)+".bmp")
+        os.rename(new_file_path_pic+"img.png",new_file_path_pic+str(i+1)+".png")
         
     else:
         pass
+
 
